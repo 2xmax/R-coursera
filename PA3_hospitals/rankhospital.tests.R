c30002f2a468d17daeabe06bafd266b2c4125402
Maxim Maximov <2xmax@mail.ru>
2015-01-11 23:17:08 +0300

    Removed redundant test

--- PA3_hospitals/rankhospital.tests.R
+++ PA3_hospitals/rankhospital.tests.R
@@ -6,7 +6,5 @@ checkException(rankhospital("INVALID", "heart failure", 4), "invalid state")
 checkException(rankhospital("TX", "INVALID", 4), "invalid outcome")
 
 checkEquals("DETAR HOSPITAL NAVARRO", rankhospital("TX", "heart failure", 4))
-
-checkEquals("DETAR HOSPITAL NAVARRO", rankhospital("TX", "heart failure", 4))
 checkEquals("HARFORD MEMORIAL HOSPITAL", rankhospital("MD", "heart attack", "worst"))
 checkTrue(is.na(rankhospital("MN", "heart attack", 5000)))
